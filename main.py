ef518b9862b644d1c25a9dee638146b4cd256389
Snezhana <nana-1980@mail.ru>
2024-07-07 16:49:51 +0300

    add beckapik1

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import logging
 from executive_functions.function import function
 from API.create_a_folder import create_a_folder
+from Config.config_file import interval
 
 
 if __name__ == '__main__':
     logging.info('Программма синхронизации запущена')
     create_a_folder()
     while True:
-        function(10)
+        function(interval)
