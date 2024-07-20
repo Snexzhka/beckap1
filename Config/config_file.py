ebc2a8f0210a06f6e86110c30b78e71d0e235897
Snexzhka <162602511+Snexzhka@users.noreply.github.com>
2024-07-20 23:11:08 +0300

    Update config_file.py

--- Config/config_file.py
+++ Config/config_file.py
@@ -3,7 +3,7 @@ import configparser
 config = configparser.ConfigParser()
 
 config.add_section('Keys')
-config.set('Keys', 'TOKEN', 'y0_AgAAAAB2xH3PAADLWwAAAAEHzJX3AADjH8uX2w9E6LT-9Y4ULbB-gAuWNQ')
+config.set('Keys', 'TOKEN', 'y')
 config.set("Keys", 'disk_path', 'backup')
 config.set('Keys', 'local_file', r'D:\test')
 config.set('Keys', 'interval', '20')
